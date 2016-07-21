a554fd7fd54ac7c638ed57304b5b66f6a5b2e74f
Ben Radovitzky <bradovitzky@isbos.org>
2016-07-21 13:20:56 -0400

    Changes to be committed: 	modified:   Practice.py

--- Practice.py
+++ Practice.py
@@ -13,19 +13,19 @@ class Edge(object):
 
 class CreateGraph(object):
     def __init__(self):
-        self.nodes = []
+        self.nodes = {}
         self.edges = {}
 
     def add_node(self, name, height):
         node = Node(name, height)
-        self.nodes.append(node)
-        return node
+        self.nodes[name] = height
+        return
 
     def add_edge(self, source, sink, capacity):
         edge = Edge(source, sink, capacity)
-        self.edges[source, sink] = capacity
+        self.edges[edge] = capacity
 
-    def make_it_flow(self, source, sink):
+    def make_it_flow(self, source, sink):   x
         for edge in self.edges: #For every edge in the graph
             if self.edges[source, sink] > sink.h: #if the source of the edge has more packets than the sink
                 if self.edges[source, sink] >= source.h: #and if the capacity of the edge is greater than the height of the source
@@ -37,12 +37,12 @@ class CreateGraph(object):
 
 if __name__ == "__main__":
     g = CreateGraph()
-    s = g.add_node(s, 4)
-    o = g.add_node(o, 4)
-    p = g.add_node(p, 4)
-    q = g.add_node(q, 4)
-    source = g.add_node(source, 1000)
-    sink = g.add_node(sink, 0)
+    s = g.add_node("s", 4)
+    o = g.add_node("o", 4)
+    p = g.add_node("p", 4)
+    q = g.add_node("q", 4)
+    source = g.add_node("source", 1000)
+    sink = g.add_node("sink", 0)
     g.add_edge(source, s, 2)
     g.add_edge(source, o, 2)
     g.add_edge(s, p, 2)
