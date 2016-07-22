9501e43694cf1633a1987b9f17d393106c5d0d99
Ben Radovitzky <bradovitzky@isbos.org>
2016-07-22 16:23:29 -0400

    modified:   NoEdges.py

--- NoEdges.py
+++ NoEdges.py
@@ -2,23 +2,25 @@ class Node(object):
     def __init__(self, name, height, ):
         self._name = name
         self._height = height
-        self._sinks = []
+        self._sinks = {}
         return
 
-    def add_sink(self, node):
-        self._sinks.append(node)
+    def add_sink(self, node, capacity):
+        self._sinks[node] = capacity
+        print("Connection created between", self._name, "and", node, "with capacity", capacity)
         return
 
     def print_all_neighbors(self):
-        myString = "My name is: {}, I have: {} sinks: ".format(self._name, len(self._sinks))
+        myString = "My name is {} and I have {} sinks: ".format(self._name, len(self._sinks))
         print(myString, ', '.join(str(p) for p in self._sinks))
         for sink in self._sinks:
             sink.print_all_neighbors()
         return
 
     def __str__(self):
-        s = self._name
-        return s
+        name = self._name
+        return name
+
 
 class Graph(object):
     def __init__(self):
@@ -26,23 +28,22 @@ class Graph(object):
 
     def add_node(self, node):
         self._nodelist.append(node)
-        print('Node "', node, '" added to nodelist')
+        print('Node "', node, '" added to graph')
 
 if __name__ == "__main__":
     source = Node("source", 1)
     s = Node("s", 2)
-    source.add_sink(s)
+    source.add_sink(s, 10)
     o = Node("o", 3)
-    source.add_sink(o)
+    source.add_sink(o, 10)
     p = Node("p", 3)
-    s.add_sink(p)
+    s.add_sink(p, 10)
     q = Node("q", 3)
-    o.add_sink(q)
-    o.add_sink(p)
+    o.add_sink(q, 10)
+    o.add_sink(p, 10)
     sink = Node("sink", 0)
-    p.add_sink(sink)
-    q.add_sink(sink)
-    print("no problem up to here")
+    p.add_sink(sink, 10)
+    q.add_sink(sink, 10)
     source.print_all_neighbors()
 
     g = Graph()
