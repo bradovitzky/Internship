97793324ead6483c41d05ce112c3d74f275235d3
Ben Radovitzky <bradovitzky@isbos.org>
2016-07-26 09:19:54 -0400

    modified:   NoEdges.py

--- NoEdges.py
+++ NoEdges.py
@@ -1,5 +1,6 @@
+from itertools import cycle
 class Node(object):
-    def __init__(self, name, height, ):
+    def __init__(self, name, height):
         self._name = name
         self._height = height
         self._sinks = {}
@@ -25,20 +26,49 @@ class Node(object):
 class Graph(object):
     def __init__(self):
         self._nodelist = []
+        self._path = []
 
     def add_node(self, node):
         self._nodelist.append(node)
         print('Node "', node, '" added to graph')
 
+    def sand_pile(self):
+        listcycle = cycle(self._nodelist)
+        start = next(listcycle)
+        nextnode = next(listcycle)
+        print(nextnode)
+        while nextnode != source:
+            if nextnode in start._sinks:
+                print(nextnode, "in node._sinks")
+                if start._height > nextnode._height:
+                    print(start._sinks[nextnode])
+                    print(start._height)
+                    print(nextnode._height)
+                    print("connection capacity:", start._sinks[nextnode])
+                    if start._sinks[nextnode] >= start._height:
+                        print(nextnode._height, "packets transferred from", start._name, "to", nextnode._name)
+                        start._height = 0
+                        nextnode._height += start._height
+                    elif start._sinks[nextnode] < start._height:
+                        start._height -= start._sinks[nextnode]
+                        nextnode._height += start._sinks[nextnode]
+                        print(nextnode._height, "packets transferred to", nextnode._name, "from", start._name)
+                        print("The height of", nextnode._name, "is", nextnode._height, "and the height of", start._name, "is", start._height)
+                    else:
+                        print("lolwut")
+            nextnode = next(listcycle)
+        nextnode = start
+
+
 if __name__ == "__main__":
-    source = Node("source", 1)
-    s = Node("s", 2)
+    source = Node("source", 10000)
+    s = Node("s", 0)
     source.add_sink(s, 10)
-    o = Node("o", 3)
+    o = Node("o", 0)
     source.add_sink(o, 10)
-    p = Node("p", 3)
+    p = Node("p", 0)
     s.add_sink(p, 10)
-    q = Node("q", 3)
+    q = Node("q", 0)
     o.add_sink(q, 10)
     o.add_sink(p, 10)
     sink = Node("sink", 0)
@@ -52,4 +82,4 @@ if __name__ == "__main__":
     g.add_node(o)
     g.add_node(p)
     g.add_node(q)
-
+    g.sand_pile()
